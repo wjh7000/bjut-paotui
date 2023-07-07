98f688eeb6140d0e656ef7cac1e0e153c57218e6
RedTie <atie123456@163.com>
2023-07-07 09:28:22 +0800

    修改message中变量名bug

--- miniprogram/pages/message/message.js
+++ miniprogram/pages/message/message.js
@@ -109,7 +109,7 @@ Page({
 
         //更新用户最近读的时间
         db.collection('chat_record').where({
-            _id:this.data.my_customers[index]._id
+            _id:this.data.me_rider[index]._id
          }).update({
              data: {
                  user_read_time: myDate.toLocaleString()
